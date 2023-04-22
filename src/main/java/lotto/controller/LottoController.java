ec8dcd92b97fce1aefa3f1ac4812ea38038881c2
jongwonpc <c3392302@naver.com>
2023-04-23 05:05:39 +0900

    feat: 컨트롤러 구현

---
+++ src/main/java/lotto/controller/LottoController.java
@@ -0,0 +1,22 @@
+package lotto.controller;
+
+import lotto.domain.LottoTickets;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
+public class LottoController {
+
+    public void run(){
+        String money = InputView.inputLottoMoney();
+        OutputView.printLottoCount(money);
+
+        int lottoCount = Integer.parseInt(money) / 1000;
+        LottoTickets lottoTickets = new LottoTickets(lottoCount);
+        OutputView.printLottoTickets(lottoTickets);
+    }
+
+    public static void main(String[] args) {
+        LottoController lc = new LottoController();
+        lc.run();
+    }
+}
