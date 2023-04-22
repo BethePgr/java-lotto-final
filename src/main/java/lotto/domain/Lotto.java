4aa34047b226ce715996edab96d54b8514ff0de1
jongwonpc <c3392302@naver.com>
2023-04-23 04:43:23 +0900

    feat : 로또 예외처리 기능 구현

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -10,11 +10,19 @@ public class Lotto {
         this.numbers = numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+    private void validate(List<Integer> numbers){
+        if(validateSize(numbers) && validateDistinctSize(numbers)){
+            return;
         }
+        throw new IllegalArgumentException("[ERROR] 중복되지 않은 6개의 숫자들만 하나의 로또가 될 수 있습니다.");
+    }
+
+    private boolean validateSize(List<Integer> numbers) {
+        return numbers.size() == 6;
     }
 
     // TODO: 추가 기능 구현
+    private boolean validateDistinctSize(List<Integer> numbers){
+        return numbers.stream().distinct().count() == 6;
+    }
 }
