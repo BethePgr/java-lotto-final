71791c79260e80a324eca0a08c6684942a9bd7ed
jongwonpc <c3392302@naver.com>
2023-04-23 04:40:22 +0900

    feat : 구입한 로또의 개수만큼 로또 발급해주는 lottoTickets 구현

---
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -0,0 +1,27 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LottoTickets {
+
+    private List<Lotto> lottoTickets;
+
+    public LottoTickets(int lottoMoney){
+        makeLottoTickets(lottoMoney);
+    }
+
+    public void makeLottoTickets(int lottoMoney){
+        List<Lotto> tempLottoTickets = new ArrayList<>();
+        for(int i = 0;i<lottoMoney;i++){
+            List<Integer> tempLotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1,45,6));
+            Collections.sort(tempLotto);
+            Lotto lotto = new Lotto(tempLotto);
+            tempLottoTickets.add(lotto);
+        }
+        lottoTickets = tempLottoTickets;
+    }
+
+}
