1376b9d9ebeaa8f9f6eb5c77666a4a84043a6e5c
jongwonpc <c3392302@naver.com>
2023-04-25 06:09:50 +0900

    feat: 로또 결과 기능 구현

--- src/main/java/lotto/domain/LottoResult.java
+++ src/main/java/lotto/domain/LottoResult.java
@@ -13,38 +13,43 @@ public class LottoResult {
     private final LottoTickets lottoTickets;
     private final List<Integer> lottoAnswer;
     private final int bonusNumber;
+    private final Map<LottoRank,Integer> map = new EnumMap<LottoRank, Integer>(LottoRank.class);
 
     public LottoResult(LottoTickets lottoTickets, List<Integer> lottoAnswer, int bonusNumber) {
         this.lottoTickets = lottoTickets;
         this.lottoAnswer = lottoAnswer;
         this.bonusNumber = bonusNumber;
+        initMap();
+        createMap();
     }
 
-    public void createMap(LottoTickets lottoTickets, List<Integer> lottoAnswer, int bonusNumber) {
-        Map<LottoRank, Integer> map = new EnumMap<>(LottoRank.class);
-
-        for (Lotto lottoTicket : lottoTickets.getLottoTickets()) {
-            List<Integer> numbers = lottoTicket.getNumbers();
-            int count = (int) numbers.stream()
-                .filter(num -> lottoAnswer.stream().anyMatch(Predicate.isEqual(num))).count();
-            boolean bonus = lottoAnswer.contains(bonusNumber);
-            LottoRank lottoRank = Arrays.stream(LottoRank.values())
-                .filter(num -> LottoRank.checkLottoRank(num, count, bonus)).findAny()
-                .orElse(LottoRank.None);
-
+    public void initMap(){
+        List<LottoRank> lottoranks = Arrays.stream(LottoRank.values()).collect(Collectors.toList());
+        for(LottoRank lottoRank: lottoranks){
+            map.put(lottoRank,0);
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(LottoRank.values()));
-        boolean flag = true;
-        LottoRank lottoRank = Arrays.stream(LottoRank.values())
-            .filter(num -> LottoRank.checkLottoRank(num, 5, flag)).findAny()
+
+    public void createMap() {
+        for (Lotto lottoTicket : lottoTickets.getLottoTickets()) {
+            List<Integer> numbers = lottoTicket.getNumbers();
+            LottoRank lottoRank = getLottoRank(numbers, lottoAnswer, bonusNumber);
+            map.put(lottoRank, map.get(lottoRank)+1);
+        }
+    }
+
+    private LottoRank getLottoRank(List<Integer> numbers, List<Integer> lottoAnswer,
+        int bonusNumber) {
+        int count = (int) numbers.stream()
+            .filter(num -> lottoAnswer.stream().anyMatch(Predicate.isEqual(num))).count();
+        boolean bonus = lottoAnswer.contains(bonusNumber);
+        return Arrays.stream(LottoRank.values())
+            .filter(num -> LottoRank.checkLottoRank(num, count, bonus)).findAny()
             .orElse(LottoRank.None);
-        LottoRank lottoRank2 = Arrays.stream(LottoRank.values())
-            .filter(num -> LottoRank.checkLottoRank(num, 5, !flag)).findAny()
-            .orElse(LottoRank.None);
-        System.out.println(lottoRank);
-        System.out.println(lottoRank2);
+    }
+
+    public Map<LottoRank,Integer> getMap(){
+        return map;
     }
 }
