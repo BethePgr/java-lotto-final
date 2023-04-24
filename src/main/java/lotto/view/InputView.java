d3b6d75f441fb739d3918b08b0e984cda35ece9f
SEC <c3392302@naver.com>
2023-04-24 11:04:01 +0900

    feat: 로또 당첨 번호를 리스트 형식으로 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,30 +1,34 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import lotto.validate.InputViewValidation;
 
 public class InputView {
 
-    public static String inputLottoMoney(){
+    public static String inputLottoMoney() {
         System.out.println("구입금액을 입력해 주세요.");
         String input = Console.readLine();
         InputViewValidation.checkInputMoney(input);
         return input;
     }
 
-    public static String inputLottoAnswer(){
+    public static List<Integer> inputLottoAnswer() {
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
         InputViewValidation.checkLottoAnswer(input);
-        return input;
+        return Arrays.stream(input.split(",")).map(num -> Integer.parseInt(num))
+            .collect(Collectors.toList());
     }
 
-    public static String inputLottoBonus(String lottoAnswer){
+    public static String inputLottoBonus(String lottoAnswer) {
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요");
         String input = Console.readLine();
-        InputViewValidation.checkLottoBonus(input,lottoAnswer);
+        InputViewValidation.checkLottoBonus(input, lottoAnswer);
         return input;
     }
 }
