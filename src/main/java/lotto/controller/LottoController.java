6a1ec9ddde8efac33add9e9038c78a02c9f9b727
SEC <c3392302@naver.com>
2023-04-25 12:13:58 +0900

    refactor: 컨트롤러 리팩토링

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -17,18 +17,29 @@ public class LottoController {
     }
 
     private void runNoTC(){
-        String money = InputView.inputLottoMoney();
-        OutputView.printLottoCount(money);
-
+        String money = printMoney();
         int lottoCount = Integer.parseInt(money) / 1000;
-        LottoTickets lottoTickets = new LottoTickets(lottoCount);
-        OutputView.printLottoTickets(lottoTickets);
-
+        LottoTickets lottoTickets = printLottoTickets(lottoCount);
         List<Integer> lottoAnswer = InputView.inputLottoAnswer();
         int lottoBonus = InputView.inputLottoBonus(lottoAnswer);
         LottoResult lottoResult = new LottoResult(lottoTickets, lottoAnswer, lottoBonus);
+        printLottoResults(lottoResult,money);
+    }
+
+    private String printMoney(){
+        String money = InputView.inputLottoMoney();
+        OutputView.printLottoCount(money);
+        return money;
+    }
+
+    private LottoTickets printLottoTickets(int lottoCount){
+        LottoTickets lottoTickets = new LottoTickets(lottoCount);
+        OutputView.printLottoTickets(lottoTickets);
+        return lottoTickets;
+    }
+
+    private void printLottoResults(LottoResult lottoResult, String money){
         OutputView.printLottoResults(lottoResult);
         OutputView.printBenefit(Integer.parseInt(money),lottoResult);
     }
-
 }
