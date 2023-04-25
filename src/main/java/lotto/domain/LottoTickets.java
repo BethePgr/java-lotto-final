d9db9cb15e9a46fc1d8ee6ac5ad422eb5d779747
SEC <c3392302@naver.com>
2023-04-25 12:24:06 +0900

    refactor: lotto 하나를 만드는 메서드 리팩토링

--- src/main/java/lotto/domain/LottoTickets.java
+++ src/main/java/lotto/domain/LottoTickets.java
@@ -9,22 +9,25 @@ public class LottoTickets {
 
     private List<Lotto> lottoTickets;
 
-    public LottoTickets(int lottoMoney){
+    public LottoTickets(int lottoMoney) {
         makeLottoTickets(lottoMoney);
     }
 
-    public void makeLottoTickets(int lottoMoney){
+    public void makeLottoTickets(int lottoMoney) {
         List<Lotto> tempLottoTickets = new ArrayList<>();
-        for(int i = 0;i<lottoMoney;i++){
-            List<Integer> tempLotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1,45,6));
-            Collections.sort(tempLotto);
-            Lotto lotto = new Lotto(tempLotto);
-            tempLottoTickets.add(lotto);
+        for (int i = 0; i < lottoMoney; i++) {
+            tempLottoTickets.add(makeOneLotto());
         }
         lottoTickets = tempLottoTickets;
     }
 
-    public List<Lotto> getLottoTickets(){
+    private Lotto makeOneLotto(){
+        List<Integer> tempLotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        Collections.sort(tempLotto);
+        return new Lotto(tempLotto);
+    }
+
+    public List<Lotto> getLottoTickets() {
         return lottoTickets;
     }
 }
